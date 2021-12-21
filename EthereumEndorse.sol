ddaea15102f266523730e4f328804c6bda39c349
Lichen <59519021+Lichen-Dev@users.noreply.github.com>
2021-12-21 17:20:02 +0000

    Update EthereumEndorse.sol

--- EthereumEndorse.sol
+++ EthereumEndorse.sol
@@ -1,5 +1,14 @@
 pragma solidity ^0.8.10;
 
+
+interface NFT{
+    
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+    function ownerOf(uint256 tokenId) external view returns (address) ;
+
+}
+
+
 contract EthereumEndorse {
 
     struct SignObject{
@@ -13,30 +22,49 @@ contract EthereumEndorse {
 
  mapping(string => SignObject) public SignObjects;
 
- function sign(string memory URI) public {
+
+
+
+
+function GetOwner(uint256 tokenId, address NFTAddress) public returns (address){
+
+   return NFT(NFTAddress).ownerOf(tokenId);
+}
+
+function sign(address NFTAddress, uint256 tokenId, string memory Concatenated) public {
      uint a;
      bool SenderSigned;
-    SignObject storage SignObject_ = SignObjects[URI];
+     address owner;
+
+    owner = NFT(NFTAddress).ownerOf(tokenId);
+
+    SignObject storage SignObject_ = SignObjects[Concatenated];
 
     if (SignObject_.setvalues!=true){
             SignObject_.setvalues=true;
-            SignObject_.URI=URI;
-
     }
-    for(a=0; a<100; a++){
+
+    for(a=1; a<99; a++){
+
         if (SignObject_.Signatures[a]==msg.sender){
             SenderSigned=true;
         }
     }
-    if (SenderSigned!= true){
+
+    if(SenderSigned != true){
+        
         SignObject_.ArrayPosition++;
         SignObject_.Signatures[SignObject_.ArrayPosition]=msg.sender;
+    
     }
     }
 
- function GetSignatures(string memory URI) public returns(address[99] memory){
-     SignObject storage SignObject_ = SignObjects[URI];
+ 
+ function GetSignatures(string memory Concatenated) public returns(address[99] memory){
+     SignObject storage SignObject_ = SignObjects[Concatenated];
      return SignObject_.Signatures;
  }
 
 }
+
+
